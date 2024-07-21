7ad5f79192902b0065d60a207e56c4c5d99f2278
Liiiingb <141879025+Liiiingb@users.noreply.github.com>
2024-07-22 00:33:31 +1200

    Update App.js link to aut.ac.nz

--- worksheet1/cise-react-learn/src/App.js
+++ worksheet1/cise-react-learn/src/App.js
@@ -10,13 +10,13 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-  className="App-link"
-  href="https://aut.ac.nz"
-  target="_blank"
-  rel="noopener noreferrer"
->
-  Learn React
-</a>
+          className="App-link"
+          href="https://aut.ac.nz"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
 
       </header>
     </div>
